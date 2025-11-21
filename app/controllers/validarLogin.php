939166ab38a01cb83a3b8974d013e45e7b57b33b
AndresGovea <goveaandres11@gmail.com>
2025-11-21 09:28:13 -0600

    chore: Coming back to a correct version

--- app/controllers/validarLogin.php
+++ app/controllers/validarLogin.php
@@ -1,13 +1,8 @@
 <?php
 session_start();
  include ('../models/db.php');    
-<<<<<<< HEAD
- $usuario = $_POST['email'];
- $contrasena =$_POST['contrasena'];
-=======
  $email = $_POST['email'];
  $contrasena =$_POST['password'];
->>>>>>> 6a257d57f428af64d48ad6a8470fcadf64a2b656
 
 $q = "SELECT COUNT(*) as contar FROM usuario WHERE email ='$email' AND contrasena ='$contrasena'";
 
