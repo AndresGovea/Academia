d1969de5703390b6a6fe2c9ea6e1986416fbcc31
AndresGovea <goveaandres11@gmail.com>
2025-11-29 19:27:43 -0600

    feat: add admin page with read-only access

--- app/view/admin.php
+++ app/view/admin.php
@@ -1,20 +1,65 @@
+<?php
+    require_once('../models/db.php');
+    $query = "SELECT id_usuario, nombre, ap_paterno, ap_materno, email, rol FROM usuario";
+    $result = $conn->query($query);
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es-MX">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="../../public/img/school.svg">
-    <title>Administrar</title>
+    <title>Administrar - Academia</title>
 </head>
 <body>
-    <?php include ('layout/header.php');?>
-
-
-
-
-    
+<?php include ('layout/header.php');?>
+    <div class="container fluid">
+        <div class="row mt-5">
+            <div class="col">
+                <h1>Usuarios <a href="#" class="btn btn-primary">Agregar</a></h1>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Apellido paterno</th>
+                            <th scope="col">Apellido materno</th>
+                            <th scope="col">Correo Electrónico</th>
+                            <th scope="col">Rol</th>
+                            <th scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while($row = $result->fetch_assoc()) {
+                        ?>
+                        <tr>
+                            <td><?php echo $row['id_usuario']; ?></td>
+                            <td><?php echo $row['nombre']; ?></td>
+                            <td><?php echo $row['ap_paterno']; ?></td>
+                            <td><?php echo $row['ap_materno']; ?></td>
+                            <td><?php echo $row['email']; ?></td>
+                            <td><?php echo $row['rol']; ?></td>
+                            <td>
+                                <a href="#" class="btn btn-warning">Editar</a>
+                                <a href="#" class="btn btn-danger">Eliminar</a>
+                            </td>
+                        </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     <?php include ('layout/footer.php');?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
